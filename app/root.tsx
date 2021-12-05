03650954d7473c16507f48d062b2a6eff4174d1e
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-05 20:14:14 +0530

    hello world in remix

---
+++ app/root.tsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <title>Hello</title>
+      </head>
+      <body>
+        <div>
+          <h1>HELLO WORLD!!!</h1>
+        </div>
+      </body>
+    </html>
+  );
+};
+
+export default App;
