d84a2b38805be8c322ada555553921d5c2403233
Sidharth Mohanty <sidmohanty11@gmail.com>
2021-12-13 23:56:46 +0530

    prisma and db connect + fetch

--- app/routes/jokes/index.tsx
+++ app/routes/jokes/index.tsx
@@ -1,9 +1,32 @@
+import { db } from "~/utils/db.server";
+import { LoaderFunction, useLoaderData } from "remix";
+
 const Jokes = () => {
+  const data = useLoaderData();
   return (
     <div>
       <h1>All Jokes !!!</h1>
+      <p>
+        {data.jokeListItems.map((j) => (
+          <li key={j.id}>{j.name}</li>
+        ))}
+      </p>
     </div>
   );
+};
+
+type LoaderData = {
+  jokeListItems: {
+    id: string;
+    name: string;
+  }[];
+};
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    jokeListItems: await db.joke.findMany(),
+  };
+  return data;
 };
 
 export default Jokes;
